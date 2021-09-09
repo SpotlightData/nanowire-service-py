73939994d32b45cf8b5750021fea994c8fddc485
WhoAteDaCake <doitsimplefy@gmail.com>
2021-09-09 14:13:22 +0100

    Feat: try to export ypes

--- nanowire_service_py/__init__.py
+++ nanowire_service_py/__init__.py
@@ -1,7 +1,7 @@
 # type: ignore[attr-defined]
 """Wrapper for interacting with Nanowire platform"""
 from .types import *
-from .service import ServiceClient, Worker
+from .service import *
 from .utils import *
 from .logger import Logger
 
@@ -14,3 +14,21 @@ try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
+
+__all__ = [
+    "Logger",
+    "ServiceClient",
+    "Worker",
+    "DaprRequestBody",
+    "Path",
+    "OrderedInstance",
+    "Task",
+    "ChildAction",
+    "BaseActions",
+    "Actions",
+    "PluginOutput",
+    "TaskOutput",
+    "MutliInput",
+    "GroupedInput",
+    "MultiInputTask",
+]
